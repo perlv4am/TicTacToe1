5d32f3d3c695c4e3473afc5bcbb787cf4a895bf4
Vaclav Perlik <vaclav.perlik@gmail.com>
2018-09-14 14:25:37 +0200

    Add project files.

---
+++ TicTacToe1/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe_Game game1 = new TicTacToe_Game(3);
+            game1.Play();
+        }
+
+
+    }
+
+ 
+
+
+}
---
+++ TicTacToe1/TicTacToe_Game.cs
@@ -0,0 +1,548 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TicTacToe1
+{
+    public class TicTacToe_Game
+    {
+        //Class that includes all information and methods about the game
+
+        //initilize variables
+        public int dimensionOfTheField;  //Dimension of the grid
+        int[,] gameField;
+        public bool gameEnded = false;
+
+        //Constructor for the game
+        public TicTacToe_Game(int dimension)
+        {
+            dimensionOfTheField = dimension;
+            //gameField = new int[dimensionOfTheField, dimensionOfTheField];
+            gameField = new int[,] {{0,0,2},{0,0,0},{0,0,0},{0,0,0}};
+        }
+
+        //Human has crosses
+        public int AddCross(int[] Coordinates, TicTacToe_Game game)
+        {
+            int X = Coordinates[0];
+            int Y = Coordinates[1];
+            if (game.gameField[X, Y] == 0)
+            {
+                game.gameField[X, Y] = 1;
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //Computer has rings
+        public int AddRing(int[] Coordinates)
+        {
+            int X = Coordinates[0];
+            int Y = Coordinates[1];
+
+            if (gameField[X, Y] == 0)
+            {
+                gameField[X, Y] = 2;
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //Read players input
+        public void GetInput()
+        {
+            Console.WriteLine("Zadej X souradnici");
+            int X_input = Convert.ToInt32(Console.ReadLine());
+
+            if (X_input >= dimensionOfTheField)
+                Console.WriteLine("Zahral jsi out");
+
+            Console.WriteLine("Zadej Y souradnici");
+            int Y_input = Convert.ToInt32(Console.ReadLine());
+
+            if (X_input >= dimensionOfTheField)
+                Console.WriteLine("Zahral jsi out");
+
+            int[] Vstup = new int[] { X_input, Y_input };
+            AddCross(Vstup, this);
+
+            if ((WhoWins(gameField) == 1) || (WhoWins(gameField) == 2) || (WhoWins(gameField) == 0))
+            {
+                gameEnded = true;
+            }
+        }
+
+        //Print current game in console
+        public void PrintGame()
+        {
+            Console.Clear();
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                Console.WriteLine(new string('-', 2 * dimensionOfTheField));
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    Console.Write('|');
+                    Console.Write(Convert.ToString(gameField[ii, jj]));
+                }
+                Console.Write('|');
+                Console.Write('\n');
+            }
+        }
+
+
+        public void ComputerTurn()
+        {
+            int[] coordinates;
+            int[,] score = new int[dimensionOfTheField, dimensionOfTheField];
+            int[,] temporalField = new int[dimensionOfTheField, dimensionOfTheField];
+
+            Parallel.For(0, dimensionOfTheField, ii =>
+          {
+              for (int jj = 0; jj < dimensionOfTheField; jj++)
+              {
+                  if (gameField[ii, jj] == 0)
+                  {
+                      temporalField = (int[,])gameField.Clone();
+                      temporalField[ii, jj] = 2;
+
+                        //Check if it is a winnig turn
+                        if (WhoWins(temporalField) == 2)
+                      {
+                          score[ii, jj] = 10;
+                      }
+
+                        //Check if it is a loosing turn
+                        if (WhoWins(temporalField) == 1)
+                      {
+                          score[ii, jj] = -10;
+                      }
+
+                        //Check if it is a draw game
+                        if (WhoWins(temporalField) == 0)
+                      {
+                          score[ii, jj] = 0;
+                      }
+
+                      score[ii, jj] = WhereToPlay(1, temporalField);
+                  }
+                  else
+                  {
+                      score[ii, jj] = 6;
+                  }
+              }
+          });            
+            AddRing(FindMaximumCoordinates(score));
+            if ((WhoWins(gameField) == 1) || (WhoWins(gameField) == 2) || (WhoWins(gameField) == 0))
+            {
+                gameEnded = true;
+            }
+        }
+
+        //Methods returns values of possible turns
+        public int WhereToPlay(int player, int[,] gameField)
+        {
+            int[,] temporalField;
+            int[] coordinates = new int[] { 0, 0 };
+            int[,] score = new int[dimensionOfTheField, dimensionOfTheField];
+
+            //Try all possibilities
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    if (gameField[ii, jj] == 0)
+                    {
+                        temporalField = (int[,])gameField.Clone();
+                        temporalField[ii, jj] = player;
+
+                        //Check if it is a winnig turn
+                        if (WhoWins(temporalField) == 2)
+                        {
+                            return (10);
+                        }
+
+                        //Check if it is a loosing turn
+                        if (WhoWins(temporalField) == 1)
+                        {
+                            return (-10);
+                        }
+
+                        //Check if it is a draw game
+                        if (WhoWins(temporalField) == 0)
+                        {
+                            return (0);
+                        }
+
+                        //If it is not a winnig turn than evaluate other player's possibilities
+                        if (player == 1)
+                        {
+                            score[ii, jj] = WhereToPlay(2, temporalField);
+                        }
+                        else
+                        {
+                            score[ii, jj] = WhereToPlay(1, temporalField);
+                        }
+                    }
+                    else
+                    {
+                        score[ii, jj] = 6;
+                    }
+                }
+            }
+            return (EvaluatePosition(score, player));
+        }
+
+
+
+        //
+        private int EvaluatePosition(int[,] score, int player)
+        {
+
+            if (player == 1)
+            {
+                return (FindMinimumValue(score));
+            }
+            else
+            {
+                return (FindMaximumValue(score));
+            }
+        }
+
+        //Function looks for maximum number in 2D array
+        private int FindMaximumValue(int[,] score)
+        {
+            int maximum = -1000;
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    if ((score[ii, jj] > maximum) && (score[ii, jj] != 6))
+                    {
+                        maximum = score[ii, jj];
+                    }
+                }
+            }
+            return (maximum);
+        }
+
+        //Function looks for coordinates of the maximum number in 2D array
+        private int[] FindMaximumCoordinates(int[,] score)
+        {
+            int[] coordinates = new int[2];
+            int maximum = -1000;
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    if ((score[ii, jj] > maximum)&&(score[ii, jj] != 6))
+                    {
+                        maximum = score[ii, jj];
+                        coordinates[0] = ii;
+                        coordinates[1] = jj;
+                    }
+                }
+            }
+            return (coordinates);
+        }
+
+        //Function looks for minimum number in 2D array
+        private int FindMinimumValue(int[,] score)
+        {
+            int minimum = 1000;
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    if ((score[ii, jj] < minimum)&& (score[ii, jj] != 6))
+                    {
+                        minimum = score[ii, jj];
+                    }
+                }
+            }
+            return (minimum);
+        }
+
+
+        //Function looks for coordinates of the minimum number in 2D array
+        private int[] FindMinimumCoordinates(int[,] score)
+        {
+            int[] coordinates = new int[2];
+            int minimum = 1000;
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    if ((score[ii, jj] < minimum)&& (score[ii, jj] != 6))
+                    {
+                        minimum = score[ii, jj];
+                        coordinates[0] = ii;
+                        coordinates[1] = jj;
+                    }
+                }
+            }
+            return (coordinates);
+        }
+
+        public int WhoWins(int[,] gameField)
+        {
+            int length_Human = 0;
+            int length_Computer = 0;
+
+            //Check rows
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    if (gameField[ii, jj] == 1)
+                    {
+                        length_Human++;
+                        length_Computer = 0;
+                    }
+                    else if (gameField[ii, jj] == 2)
+                    {
+                        length_Human = 0;
+                        length_Computer++;
+                    }
+                    else
+                    {
+                        length_Computer = 0;
+                        length_Human = 0;
+                    }
+
+                    if (length_Human == 3)
+                        return 1;
+                    if (length_Computer == 3)
+                        return 2;
+                }
+                length_Computer = 0;
+                length_Human = 0;
+            }
+
+
+
+            //Check columns
+            for (int jj = 0; jj < dimensionOfTheField; jj++)
+            {
+                for (int ii = 0; ii < dimensionOfTheField; ii++)
+                {
+
+                    if (gameField[ii, jj] == 1)
+                    {
+                        length_Human++;
+                        length_Computer = 0;
+                    }
+                    else if (gameField[ii, jj] == 2)
+                    {
+                        length_Human = 0;
+                        length_Computer++;
+                    }
+                    else
+                    {
+                        length_Computer = 0;
+                        length_Human = 0;
+                    }
+
+                    if (length_Human == 3)
+                        return 1;
+                    if (length_Computer == 3)
+                        return 2;
+                }
+                length_Computer = 0;
+                length_Human = 0;
+            }
+
+
+
+            //Check diagonal
+            for (int ii = dimensionOfTheField - 1; ii >= 0; ii--)
+            {
+                for (int jj = 0; ii + jj < dimensionOfTheField; jj++)
+                {
+                    if (gameField[ii + jj, dimensionOfTheField - 1 - jj] == 1)
+                    {
+                        length_Human++;
+                        length_Computer = 0;
+                    }
+                    else if (gameField[ii + jj, dimensionOfTheField - 1 - jj] == 2)
+                    {
+                        length_Human = 0;
+                        length_Computer++;
+                    }
+                    else
+                    {
+                        length_Computer = 0;
+                        length_Human = 0;
+                    }
+
+                    if (length_Human == 3)
+                        return 1;
+                    if (length_Computer == 3)
+                        return 2;
+                }
+                length_Computer = 0;
+                length_Human = 0;
+            }
+
+
+
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; dimensionOfTheField - 1 - jj - ii >= 0; jj++)
+                {
+                    if (gameField[jj, dimensionOfTheField - 1 - jj - ii] == 1)
+                    {
+                        length_Human++;
+                        length_Computer = 0;
+                    }
+                    else if (gameField[jj, dimensionOfTheField - 1 - jj - ii] == 2)
+                    {
+                        length_Human = 0;
+                        length_Computer++;
+                    }
+                    else
+                    {
+                        length_Computer = 0;
+                        length_Human = 0;
+                    }
+
+                    if (length_Human == 3)
+                        return 1;
+                    if (length_Computer == 3)
+                        return 2;
+                }
+                length_Computer = 0;
+                length_Human = 0;
+            }
+
+
+
+            //Check anti-diagonal
+            for (int ii = dimensionOfTheField - 1; ii > 0; ii--)
+            {
+                for (int jj = 0; ii + jj < dimensionOfTheField; jj++)
+                {
+                    if (gameField[ii + jj, jj] == 1)
+                    {
+                        length_Human++;
+                        length_Computer = 0;
+                    }
+                    else if (gameField[ii + jj, jj] == 2)
+                    {
+                        length_Human = 0;
+                        length_Computer++;
+                    }
+                    else
+                    {
+                        length_Computer = 0;
+                        length_Human = 0;
+                    }
+
+                    if (length_Human == 3)
+                        return 1;
+                    if (length_Computer == 3)
+                        return 2;
+                }
+                length_Computer = 0;
+                length_Human = 0;
+            }
+
+
+
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; ii + jj < dimensionOfTheField; jj++)
+                {
+                    if (gameField[jj, ii + jj] == 1)
+                    {
+                        length_Human++;
+                        length_Computer = 0;
+                    }
+                    else if (gameField[jj, ii + jj] == 2)
+                    {
+                        length_Human = 0;
+                        length_Computer++;
+                    }
+                    else
+                    {
+                        length_Computer = 0;
+                        length_Human = 0;
+                    }
+
+                    if (length_Human == 3)
+                        return 1;
+                    if (length_Computer == 3)
+                        return 2;
+                }
+                length_Computer = 0;
+                length_Human = 0;
+            }
+
+
+            //Check for draw game
+            for (int ii = 0; ii < dimensionOfTheField; ii++)
+            {
+                for (int jj = 0; jj < dimensionOfTheField; jj++)
+                {
+                    if (gameField[ii, jj] == 0)
+                    {
+                        return (7);
+                    }
+
+                }
+            }
+
+            return (0);
+        }
+
+        public void Play()
+        {
+            int winner;
+            while (true)
+            {
+                PrintGame();
+                GetInput();
+                PrintGame();
+                if (gameEnded)
+                {
+                    winner = WhoWins(gameField);
+                    break;
+                }
+                ComputerTurn();
+                PrintGame();
+                if (gameEnded)
+                {
+                    winner = WhoWins(gameField);
+                    break;
+                }
+            }
+            PrintGame();
+            switch (winner)
+            {
+                case 1:
+                    Console.WriteLine("Vyhral hrac" + winner.ToString());
+                    break;
+                case 2:
+                    Console.WriteLine("Vyhral hrac" + winner.ToString());
+                    break;
+                case 0:
+                    Console.WriteLine("Remiza");
+                    break;
+                default:
+                    Console.WriteLine("Neco se pokazilo");
+                    break;
+            }
+                    System.Threading.Thread.Sleep(3000);
+        }
+
+
+    }
+}
